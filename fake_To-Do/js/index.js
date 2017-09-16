9bab978cc7828adc2f5ea256f6f69c296641e332
yokoee <yokoee@live.com>
2017-09-16 16:04:47 +0800

    '.'

--- fake_To-Do/js/index.js
+++ fake_To-Do/js/index.js
@@ -33,20 +33,28 @@ let title = document.getElementById('title');
 title.style.top = "calc(30% - " + window.getComputedStyle(title, null).height + ")";
 
 //侧滑页
+let sideContent = document.getElementById('sideContent');
+let sideShadow = document.getElementById('sideShadow');
 document.getElementById('sideShadow').onclick = function() {
-    document.getElementById('sideContent').style.animation = 'slide-in 0.3s ease-in';
-    document.getElementById('sideContent').style.animationFillMode = 'forwards';
-    document.getElementById('sideShadow').style.animation = 'slide-in-shadow 0.3s ease-in';
-    document.getElementById('sideShadow').style.animationFillMode = 'forwards';
+    sideContent.style.animation = 'slide-in 0.3s ease-in';
+    sideContent.style.animationFillMode = 'forwards';
+    sideShadow.style.animation = 'slide-in-shadow 0.3s ease-in';
+    sideShadow.style.animationFillMode = 'forwards';
     setTimeout(() => {
         document.getElementById('sidePage').style.display = 'none';
     }, 300)
 }
 document.getElementById('openSideButton').onclick = function() {
     document.getElementById('sidePage').style.display = 'block';
-    document.getElementById('sideContent').style.animation = 'slide-out 0.3s ease-out';
-    document.getElementById('sideContent').style.animationFillMode = 'forwards';
-    document.getElementById('sideShadow').style.animation = 'slide-out-shadow 0.3s ease-in';
-    document.getElementById('sideShadow').style.animationFillMode = 'forwards';
+    sideContent.style.animation = 'slide-out 0.3s ease-out';
+    sideContent.style.animationFillMode = 'forwards';
+    sideShadow.style.animation = 'slide-out-shadow 0.3s ease-in';
+    sideShadow.style.animationFillMode = 'forwards';
+}
 
+//top bar
+let topBar = document.getElementById('topBar');
+let mainPage = document.getElementById('mainPage');
+mainPage.ontouchmove = function() {
+    console.log(event);
 }
